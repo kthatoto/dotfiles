15deed04bb0e19d7137fec682b4c517d16d7465c
kthatoto <kthatoto@gmail.com>
2025-08-03 20:15:18 +0900

    Fix lsp

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -3,31 +3,70 @@ require("mason-lspconfig").setup()
 
 local lspconfig = require("lspconfig")
 
--- 汎用 on_attach
 local on_attach = function(_, bufnr)
-  local map = function(keys, func)
-    vim.keymap.set("n", keys, func, { buffer = bufnr })
-  end
-  map("gd", vim.lsp.buf.definition)
-  map("K", vim.lsp.buf.hover)
-  map("<leader>rn", vim.lsp.buf.rename)
+  local opts = { buffer = bufnr }
+  local map = vim.keymap.set
+  map("n", "gd", vim.lsp.buf.definition, opts)
+  map("n", "gr", vim.lsp.buf.references, opts)
+  map("n", "K", vim.lsp.buf.hover, opts)
+  map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 end
 
--- TypeScript
-lspconfig.tsserver.setup({ on_attach = on_attach })
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- Vue (Volar)
-lspconfig.volar.setup({
+-- Lua
+lspconfig.lua_ls.setup({
   on_attach = on_attach,
-  filetypes = { "vue" },
+  capabilities = capabilities,
 })
 
 -- Ruby (Shopify Ruby LSP)
-lspconfig.ruby_lsp.setup({ on_attach = on_attach })
+lspconfig.ruby_lsp.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- RuboCop LSP (非推奨でなければ)
+lspconfig.rubocop.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
 
 -- Sorbet
 lspconfig.sorbet.setup({
   cmd = { "srb", "tc", "--lsp" },
   on_attach = on_attach,
+  capabilities = capabilities,
   root_dir = lspconfig.util.root_pattern("sorbet", ".git"),
+})
+
+-- TypeScript
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- Vue (Volar)
+lspconfig.vue_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- YAML
+lspconfig.yamlls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- TypeSpec (tsp)
+lspconfig.tsp_server.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- Markdown (zk)
+lspconfig.zk.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
