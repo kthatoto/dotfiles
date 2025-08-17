b2050096273dc7337f23797ff5b996cd889434d2
kthatoto <kthatoto@gmail.com>
2025-08-17 19:20:18 +0900

    Fix

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -7,30 +7,20 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
-    -- Masonと連携
     require("mason-null-ls").setup({
       ensure_installed = { "prettier" }, -- prettierをMasonで管理
       automatic_installation = true,
     })
 
-    -- null-lsの設定
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettier,
       },
     })
 
-    -- キーマップ
-    vim.keymap.set("n", "<leader>f", function()
+    -- :Format コマンドを定義
+    vim.api.nvim_create_user_command("Format", function()
       vim.lsp.buf.format({ async = true })
-    end, { desc = "Format code" })
-
-    -- 保存時に自動format
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = { "*.js", "*.ts", "*.tsx", "*.vue", "*.json", "*.css", "*.md" },
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
+    end, { desc = "Format current buffer" })
   end,
 }
